85fd38d62b9b45bdba6d4119078d8b2c0c364b0b
ZoonLogonEchon <maksim.biziuk@fau.de>
2022-05-17 12:02:25 +0200

    added print of input xml

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import argparse
+import xml.etree.ElementTree as ET
 
 def main(args):
-    pass
+    tree = ET.parse(args.path_to_xml)
+    root = tree.getroot()
+    print(ET.tostring(root).decode("utf-8"))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate xsd file from xml file')
