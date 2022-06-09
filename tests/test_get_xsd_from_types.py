17c8fec0dfe50dd25cdab8ac0a8d38e816c663a8
ZoonLogonEchon <maksim.biziuk@fau.de>
2022-06-09 21:45:29 +0200

    added sample xsd

--- tests/test_get_xsd_from_types.py
+++ tests/test_get_xsd_from_types.py
@@ -17,17 +17,45 @@ class TestXSDStrGeneration(unittest.TestCase):
             </config>
         </settings>
         """
-    sample_xml_0 ="""<?xml version="1.0"?>
+    sample_xsd_0 ="""<?xml version="1.0"?>
         <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
-        
-            <settings>
-                <projectauthor name="Daimo Nia" homepage="127.0.0.1" />
-                <projectpath base="path_base">rel path</projectpath>
-                <config>
-                    <attribute name="a"> 123 </attribute>
-                    <attribute name="b"> abc </attribute>
-                </config>
-            </settings>
+
+            <xs:complexType name="projectauthorType" >
+                <xs:attribute name="name" type="xs:string" use="required" />
+                <xs:attribute name="homepage" type="xs:string" use="required" />
+            </xs:complexType>
+
+            <xs:complexType name="projectpathType">
+                <xs:simpleContent>
+                    <xs:extension base="xs:string">
+                        <xs:attribute name="base" type="xs:string" use="required" />
+                    </xs:extension>
+                </xs:simpleContent>
+            </xs:complexType>
+
+            <xs:complexType name="attributeType">
+                <xs:simpleContent>
+                    <xs:extension base="xs:string">
+                        <xs:attribute name="name" type="xs:string" use="required" />
+                    </xs:extension>
+                </xs:simpleContent>
+            </xs:complexType>
+
+            <xs:complexType name="configType">
+                <xs:sequence>
+                    <xs:element name="attribute" type="attributeType" minOccurs="1" maxOccurs="unbounded"/>
+                </xs:sequence>
+            </xs:complexType>
+
+            <xs:complexType name="settingsType">
+                <xs:sequence>
+                    <xs:element name="projectauthor" type="projectauthorType"/>
+                    <xs:element name="projectpath" type="projectpathType"/>
+                    <xs:element name="config" type="configType"/>
+                </xs:sequence>
+            </xs:complexType>
+
+            <xs:element name="settings" type="settingsType" />
         </xs:schema>
         """
     sample_tree_0 ={
